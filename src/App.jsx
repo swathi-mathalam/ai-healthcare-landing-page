86deddc82a17927f16e9664395e8cbf24a4d58e4
Swathi <dsr91544@gmail.com>
2026-06-11 12:35:50 +0530

    Completed landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import ProtectedRoute from "./components/Protectedroute";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/dashboard" element={<h1>Dashboard (Protected)</h1>} />
-        </Route>
-        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <LandingPage />;
 }
 
 export default App;
